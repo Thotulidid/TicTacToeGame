8cc189f0c415299c086fc6ce6610bd2f50b1bf2e
ingolfurh <ingolfurhalld@gmail.com>
2014-10-23 16:11:59 +0000

    Added src/test/ folder

---
+++ src/test/java/is/ru/tictactoe/MoveTest.java
@@ -0,0 +1,14 @@
+package is.ru.tictactoe;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class MoveTest {
+
+	@Test
+	public void test() {
+		assert(true);
+	}
+
+}
---
+++ src/test/java/is/ru/tictactoe/TicTacToeGameTest.java
@@ -0,0 +1,14 @@
+package is.ru.tictactoe;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TicTacToeGameTest {
+
+	@Test
+	public void test() {
+		assert(true);
+	}
+
+}
