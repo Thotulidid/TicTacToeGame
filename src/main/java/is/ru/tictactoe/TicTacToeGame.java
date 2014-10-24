792fd91d4b6695ac7fec94d307f4431768aa81a9
ingolfurh <ingolfurhalld@gmail.com>
2014-10-24 15:06:48 +0000

    Added tests MoveToOccupiedPositionReturnsFalse, MoveToUnoccupiedPositionReturnsTrue and ConstructorThrowsExceptionForIllegalArgument to TicTacToeGameTest

--- src/main/java/is/ru/tictactoe/TicTacToeGame.java
+++ src/main/java/is/ru/tictactoe/TicTacToeGame.java
@@ -6,6 +6,19 @@ public class TicTacToeGame {
 	private ArrayList<Move> board;
 	private char whoHasTurn = 'X';
 	
+	public TicTacToeGame(){
+		this.board = new ArrayList<Move>(9);
+	}
+	
+	public TicTacToeGame(char firstPlayer){
+		if(firstPlayer != 'O' && firstPlayer != 'X')
+		{
+			throw new IllegalArgumentException("Player must be either 'X' or 'O'");
+		}
+		this.board = new ArrayList<Move>(9);
+		this.whoHasTurn = firstPlayer;
+	}
+	
 	public Move move(int position){
 		return new Move(position, whoHasTurn);
 	}
@@ -15,6 +28,10 @@ public class TicTacToeGame {
 	}
 	
 	public boolean move(Move move){
+		if(board.contains(move)){
+			return false;
+		}
+		board.add(move);
 		return true;
 	}
 	
--- src/test/java/is/ru/tictactoe/TicTacToeGameTest.java
+++ src/test/java/is/ru/tictactoe/TicTacToeGameTest.java
@@ -2,10 +2,15 @@ package is.ru.tictactoe;
 
 import static org.junit.Assert.*;
 
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
 public class TicTacToeGameTest {
 
+	@Rule
+	public ExpectedException thrown = ExpectedException.none();
+	
 	@Test
 	public void MoveReturnsCorrectMovePosition() {
 		TicTacToeGame game = new TicTacToeGame();
@@ -19,6 +24,32 @@ public class TicTacToeGameTest {
 		Move newMove = game.move(7, 'X');
 		assertEquals('X', newMove.getPlayer());
 		assertEquals(7, newMove.getPosition());
+	}
+	
+	@Test
+	public void MoveToOccupiedPositionReturnsFalse() {
+		TicTacToeGame game = new TicTacToeGame();
+		Move move = new Move(2, 'O');
+		game.move(move);
+		boolean goodMove = game.move(move);
+		assertEquals(goodMove, false);
+	}
+	
+	@Test
+	public void MoveToUnoccupiedPositionReturnsTrue() {
+		TicTacToeGame game = new TicTacToeGame();
+		Move move = new Move(2, 'O');
+		Move newMove = new Move(4, 'X');
+		game.move(move);
+		boolean goodMove = game.move(newMove);
+		assertEquals(goodMove, true);
+	}
+	
+	@Test
+	public void ConstructorThrowsExceptionForIllegalArgument(){
+		thrown.expect(IllegalArgumentException.class);
+        thrown.expectMessage("Player must be either 'X' or 'O'");
+        TicTacToeGame game = new TicTacToeGame('G');
 	}
 
 }
