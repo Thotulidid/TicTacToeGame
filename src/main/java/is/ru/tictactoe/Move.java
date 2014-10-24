a10dd8f644613b8d2c92d2191cbcf0a119760687
Björn Ingi Baldvinsson <bjornb13@ru.is>
2014-10-24 14:30:10 +0000

    Added javadoc comments for the Move class

--- src/main/java/is/ru/tictactoe/Move.java
+++ src/main/java/is/ru/tictactoe/Move.java
@@ -1,8 +1,23 @@
 package is.ru.tictactoe;
 
+/**
+ *  The <tt>Move</tt> class represents a data type for storing positions
+ *  in <tt>TicTacToeGame</tt>.
+ *  <p>
+ *  @author Þotuliðið
+ */
+
 public class Move {
 	private int position;
 	private char player;
+	
+    /**
+     * Initializes the <tt>Move</tt> object with <tt>position</tt> and <tt>player</tt>
+     * @param position the position of the move
+     * @param player the player of the move
+     * @throws IllegalArgumentException if the position is not in the range [0, 8]
+     * @throws IllegalArgumentException if the player is not 'X' or 'O'
+     */
 	public Move(int position, char player){
 		StringBuilder errorMessage = null;
 		if(position < 0 || position > 8){
@@ -25,10 +40,18 @@ public class Move {
 		this.player = player;
 	}
 	
+	/**
+     * Returns the position of the <tt>Move</tt>.
+     * @return The position of the <tt>Move</tt>.
+     */
 	public int getPosition(){
 		return this.position;
 	}
 	
+	/**
+     * Returns the player of the <tt>Move</tt>.
+     * @return The player of the <tt>Move</tt>.
+     */
 	public char getPlayer(){
 		return this.player;
 	}
