b2af6d1788ee9aaf93847536253ace38b7247d63
ingolfurh <ingolfurhalld@gmail.com>
2014-10-24 14:01:26 +0000

    Added functions to TicTacToeGame class

--- src/main/java/is/ru/tictactoe/TicTacToeGame.java
+++ src/main/java/is/ru/tictactoe/TicTacToeGame.java
@@ -1,5 +1,40 @@
 package is.ru.tictactoe;
 
+import java.util.ArrayList;
+
 public class TicTacToeGame {
+	private ArrayList<Move> board;
+	private boolean whoHasTurn;
 	
+	public Move Move(int position){
+		return null;
+	}
+	
+	public Move Move(int position, char player){
+		return null;
+	}
+	
+	public boolean Move(Move move){
+		return true;
+	}
+	
+	public char hasMove(char player){
+		return 'X';
+	}
+	
+	public boolean gameOver(){
+		return true;
+	}
+	
+	public ArrayList<Move> getBoard(){
+		return null;
+	}
+	
+	public void resetGame(){
+		return;
+	}
+	
+	public char Winner(){
+		return 'X';
+	}
 }
