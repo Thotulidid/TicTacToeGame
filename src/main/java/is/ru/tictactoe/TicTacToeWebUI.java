1e0866e1afbaf12a3407c4fbdf192a6da7b770ad
Björn Ingi Baldvinsson <bjornb13@ru.is>
2014-10-25 15:55:42 +0000

    Added reset game and html tags

--- src/main/java/is/ru/tictactoe/TicTacToeWebUI.java
+++ src/main/java/is/ru/tictactoe/TicTacToeWebUI.java
@@ -11,7 +11,7 @@ public class TicTacToeWebUI {
             public Object handle(Request request, Response response) {
             	
             	StringBuilder html = new StringBuilder();
-            	
+            	html.append("<html><head><title>TicTacToeWebGame</title></head><body>");
             	Move move;
             	try{
             		int position = Integer.parseInt(request.params(":param"));
@@ -30,13 +30,20 @@ public class TicTacToeWebUI {
             			html.append("<p style=\"font-size: 350%; font-weight: bold;\">The winner is:" + game.winner() + "</p>");
             		}
             	}
+            	else{
+            		html.append("<p style=\"font-size: 350%; font-weight: bold;\">" + game.hasMove() + "'s turn to move!</p>");
+            	}
             	for(int i = 0; i < 9; i++){
             		if(board[i] == null){
-            			html.append("<div style=\"width: 33%; height: 25%; float: left; font-size: 200%;\">&nbsp;</div>");
+            			html.append("<div id=" + i + " style=\"width: 33%; height: 25%; float: left; font-size: 200%;\">&nbsp;</div>");
             		}
             		else{
-            			html.append("<div style=\"width: 33%; height: 25%; float: left; font-size: 200%;\">" + board[i].getPlayer() + "</div>");
+            			html.append("<div id=" + i + " style=\"width: 33%; height: 25%; float: left; font-size: 200%;\">" + board[i].getPlayer() + "</div>");
             		}
+            	}
+            	html.append("</body></html>");
+            	if(game.gameOver()){
+            		game.resetGame();
             	}
             	return html.toString();
             	/*if(game.move(move)){
