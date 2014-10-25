842b4a011825cbcf26ef96551c50484c35fe751c
ingolfurh <ingolfurhalld@gmail.com>
2014-10-25 15:25:15 +0000

    Made html string for displaying TicTacToeGame status

--- src/main/java/is/ru/tictactoe/TicTacToeWebUI.java
+++ src/main/java/is/ru/tictactoe/TicTacToeWebUI.java
@@ -9,6 +9,9 @@ public class TicTacToeWebUI {
     	get(new Route(":param") {
             @Override
             public Object handle(Request request, Response response) {
+            	
+            	StringBuilder html = new StringBuilder();
+            	
             	Move move;
             	try{
             		int position = Integer.parseInt(request.params(":param"));
@@ -17,12 +20,31 @@ public class TicTacToeWebUI {
             	catch(Exception ex){
             		return "Out of bounds";
             	}
-            	if(game.move(move)){
+            	game.move(move);
+            	Move[] board = game.getBoard(); 
+            	if(game.gameOver()){
+            		if(game.winner() == 'D'){
+            			html.append("<p style=\"font-size: 350%; font-weight: bold;\">The game finished in a draw.</p>");
+            		}
+            		else{            			
+            			html.append("<p style=\"font-size: 350%; font-weight: bold;\">The winner is:" + game.winner() + "</p>");
+            		}
+            	}
+            	for(int i = 0; i < 9; i++){
+            		if(board[i] == null){
+            			html.append("<div style=\"width: 33%; height: 25%; float: left; font-size: 200%;\">&nbsp;</div>");
+            		}
+            		else{
+            			html.append("<div style=\"width: 33%; height: 25%; float: left; font-size: 200%;\">" + board[i].getPlayer() + "</div>");
+            		}
+            	}
+            	return html.toString();
+            	/*if(game.move(move)){
             		return move.getPlayer() + " played " + move.getPosition() + ".";
             	}
             	else{
             		return move.getPlayer() + " on " + move.getPosition() + " is illegal.";	
-            	}
+            	}*/
             }
         });
     }
