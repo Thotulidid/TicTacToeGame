c959239dd5ffb8687001e10b8c443ac20139a9f1
Björn Ingi Baldvinsson <bjornb13@ru.is>
2014-10-26 15:20:52 +0000

    Added more routes for spark

--- src/main/java/is/ru/tictactoe/TicTacToeWebUI.java
+++ src/main/java/is/ru/tictactoe/TicTacToeWebUI.java
@@ -36,24 +36,88 @@ public class TicTacToeWebUI {
             		html.append("<p style=\"font-size: 350%; font-weight: bold;\">" + game.hasMove() + "'s turn to move!</p>");
             	}
             	for(int i = 0; i < 9; i++){
+            		String color = "#FFF";
+            		if(i % 2 == 0){
+            			color = "#DDD";
+            		}
             		if(board[i] == null){
-            			html.append("<div id=" + i + " style=\"width: 33%; height: 25%; float: left; font-size: 200%;\">&nbsp;</div>");
+            			html.append("<a href=/" + i + " ><div id=" + i + " style=\"background-color: " + color + "; width: 33%; height: 25%; float: left; font-size: 200%;\">&nbsp;</div></a>");
             		}
             		else{
-            			html.append("<div id=" + i + " style=\"width: 33%; height: 25%; float: left; font-size: 200%;\">" + board[i].getPlayer() + "</div>");
+            			html.append("<div id=" + i + " style=\"background-color: " + color + "; width: 33%; height: 25%; float: left; font-size: 200%;\">" + board[i].getPlayer() + "</div>");
             		}
             	}
+            	html.append("<a href=/reset/game >Reset game</a>");
             	html.append("</body></html>");
             	if(game.gameOver()){
             		game.resetGame();
             	}
             	return html.toString();
-            	/*if(game.move(move)){
-            		return move.getPlayer() + " played " + move.getPosition() + ".";
+            }
+        });
+    	
+    	get(new Route("/") {
+            @Override
+            public Object handle(Request request, Response response) {
+            	
+            	StringBuilder html = new StringBuilder();
+            	html.append("<html><head><title>TicTacToeWebGame</title></head><body>");
+            	Move[] board = game.getBoard(); 
+            	if(game.gameOver()){
+            		if(game.winner() == 'D'){
+            			html.append("<p style=\"font-size: 350%; font-weight: bold;\">The game finished in a draw.</p>");
+            		}
+            		else{            			
+            			html.append("<p style=\"font-size: 350%; font-weight: bold;\">The winner is:" + game.winner() + "</p>");
+            		}
             	}
             	else{
-            		return move.getPlayer() + " on " + move.getPosition() + " is illegal.";	
-            	}*/
+            		html.append("<p style=\"font-size: 350%; font-weight: bold;\">" + game.hasMove() + "'s turn to move!</p>");
+            	}
+            	for(int i = 0; i < 9; i++){
+            		String color = "#FFF";
+            		if(i % 2 == 0){
+            			color = "#DDD";
+            		}
+            		if(board[i] == null){
+            			html.append("<a href=/" + i + " ><div id=" + i + " style=\"background-color: " + color + "; width: 33%; height: 25%; float: left; font-size: 200%;\">&nbsp;</div></a>");
+            		}
+            		else{
+            			html.append("<div id=" + i + " style=\"background-color: " + color + "; width: 33%; height: 25%; float: left; font-size: 200%;\">" + board[i].getPlayer() + "</div>");
+            		}
+            	}
+            	html.append("<a href=/reset/game >Reset game</a>");
+            	html.append("</body></html>");
+            	if(game.gameOver()){
+            		game.resetGame();
+            	}
+            	return html.toString();
+            }
+        });
+    	
+    	get(new Route("/reset/game") {
+            @Override
+            public Object handle(Request request, Response response) {
+            	game.resetGame();
+            	StringBuilder html = new StringBuilder();
+            	html.append("<html><head><title>TicTacToeWebGame</title></head><body>");
+            	Move[] board = game.getBoard(); 
+        		html.append("<p style=\"font-size: 350%; font-weight: bold;\">" + game.hasMove() + "'s turn to move!</p>");
+            	for(int i = 0; i < 9; i++){
+            		String color = "#FFF";
+            		if(i % 2 == 0){
+            			color = "#DDD";
+            		}
+            		if(board[i] == null){
+            			html.append("<a href=/" + i + " ><div id=" + i + " style=\"background-color: " + color + "; width: 33%; height: 25%; float: left; font-size: 200%;\">&nbsp;</div></a>");
+            		}
+            		else{
+            			html.append("<div id=" + i + " style=\"background-color: " + color + "; width: 33%; height: 25%; float: left; font-size: 200%;\">" + board[i].getPlayer() + "</div>");
+            		}
+            	}
+            	html.append("<a href=/reset/game >Reset game</a>");
+            	html.append("</body></html>");
+            	return html.toString();
             }
         });
     }
