339630bc2061c6821b4a7a93cd97f2053c069f94
Björn Ingi Baldvinsson <bjornb13@ru.is>
2014-10-28 22:27:16 +0000

    Fixed selenium test

--- src/selenium/java/is/ru/tictactoe/TicTacToeWebTest.java
+++ src/selenium/java/is/ru/tictactoe/TicTacToeWebTest.java
@@ -20,25 +20,33 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.Select;
 
 public class TicTacToeWebTest {
-  private WebDriver driver;
-  private String baseUrl;
-  private boolean acceptNextAlert = true;
-  private StringBuffer verificationErrors = new StringBuffer();
+	private WebDriver driver;
+	private String baseUrl;
+	private boolean acceptNextAlert = true;
+	private StringBuffer verificationErrors = new StringBuffer();
 
-  @Before
-  public void setUp() throws Exception {
-    driver = new FirefoxDriver();
-    baseUrl = "http://localhost:4567/";
-    driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-  }
+	  @Before
+	  public void setUp() throws Exception {
+	    driver = new FirefoxDriver();
+	    baseUrl = "http://localhost:4567/";
+	    driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+	  }
 
   @Test
-  public void testTicTacToeWeb() throws Exception {
+  public void testAsdf() throws Exception {
     driver.get(baseUrl + "/");
     driver.findElement(By.linkText("Reset game")).click();
     driver.findElement(By.id("4")).click();
-    driver.findElement(By.id("0")).click();
-    assertEquals("X's turn to move!", driver.findElement(By.cssSelector("p")).getText());
+    driver.findElement(By.id("8")).click();
+    driver.findElement(By.id("6")).click();
+    driver.findElement(By.id("5")).click();
+    driver.findElement(By.id("2")).click();
+    assertEquals("The winner is:X", driver.findElement(By.cssSelector("h1")).getText());
+    assertEquals("X", driver.findElement(By.id("4")).getText());
+    assertEquals("X", driver.findElement(By.id("6")).getText());
+    assertEquals("X", driver.findElement(By.id("2")).getText());
+    assertEquals("O", driver.findElement(By.id("8")).getText());
+    assertEquals("O", driver.findElement(By.id("5")).getText());
   }
 
   @After
